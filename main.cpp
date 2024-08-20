a2006291934cd54e956a62cf5a69416c21cbb931
Haniin111 <145842943+Haniin111@users.noreply.github.com>
2024-08-20 23:49:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,37 @@
 #include <pqxx/pqxx>
 using namespace std;
 
+template<typename T>
+void create(pqxx::work &W, const string &table, const string &column, const T &value) {
+    W.exec0("INSERT INTO " + table + " (" + column + ") VALUES (" + W.quote(value) + ");");
+    W.commit();
+    cout << "Inserted into " << table << ": " << column << " = " << value << endl;
+}
+
+template<typename T>
+void update(pqxx::work &W, const string &table, const string &column, int id, const T &new_value) {
+    W.exec0("UPDATE " + table + " SET " + column + " = " + W.quote(new_value) + " WHERE id = " + W.quote(to_string(id)) + ";");
+    W.commit();
+    cout << "Updated in " << table << ": ID " << id << " set " << column << " to " << new_value << endl;
+}
+
+template<typename T>
+void delete_record(pqxx::work &W, const string &table, int id) {
+    W.exec0("DELETE FROM " + table + " WHERE id = " + W.quote(to_string(id)) + ";");
+    W.commit();
+    cout << "Deleted from " << table << ": ID " << id << endl;
+}
+
+void read(pqxx::work &W, const string &table) {
+    pqxx::result R = W.exec("SELECT * FROM " + table + ";");
+    for (const auto &row : R) {
+        for (const auto &field : row) {
+            cout << field.c_str() << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main() {
     try {
         pqxx::connection C("dbname=mydatabase user=myuser password=mypassword hostaddr=127.0.0.1 port=5432");
@@ -14,12 +45,14 @@ int main() {
         }
 
         pqxx::work W(C);
-        pqxx::result R = W.exec("SELECT version();");
 
-        cout << "PostgreSQL version: " << R[0][0].c_str() << endl;
+        create<string>(W, "people", "name", "Alice");
+        read(W, "people");
+        update<string>(W, "people", "name", 1, "Bob");
+        read(W, "people");
+        delete_record<string> (W, "people", 1);
+        read(W, "people");
 
-        W.commit();
-        
     } catch (const pqxx::broken_connection &e) {
         cerr << "Connection to the database was broken: " << e.what() << endl;
         return 1;
@@ -34,3 +67,4 @@ int main() {
 
     return 0;
 }
+
